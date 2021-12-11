72687bec8f7361c157bf40cd9f9b204e97431842
Tae hoon Kim <taehoon.kim@fiflfifl.com>
2021-12-12 08:32:11 +0900

    add] set init environment.

---
+++ index.js
@@ -0,0 +1,21 @@
+const elScrollContainer = document.getElementById(
+  "container__carousel--scrolling"
+);
+
+const mouseEnter = {
+  enter: 0,
+  leave: 0,
+  type: "STAY",
+};
+
+let position = 0;
+
+console.log(elScrollContainer);
+
+// setInterval(() => {
+//   position += 100;
+//   elScrollContainer.style.transform = "translateX(-" + position + "%)";
+// }, 1000);
+
+elScrollContainer.addEventListener("onmouseup");
+elScrollContainer.addEventListener("onmouseup");
