b0296f86151a2c696f436059834f051a5366d16b
Tae hoon Kim <taehoon.kim@fiflfifl.com>
2021-12-12 09:03:13 +0900

    add] get scroll type to speed.

--- index.js
+++ index.js
@@ -1,21 +1,77 @@
+// elements
 const elScrollContainer = document.getElementById(
   "container__carousel--scrolling"
 );
 
-const mouseEnter = {
-  enter: 0,
-  leave: 0,
-  type: "STAY",
+const TYPE = {
+  STAY: "STAY",
+  LEFT: "LEFT",
+  RIGHT: "RIGHT",
+  RETURN_TO_LEFT: "RETURN_TO_LEFT",
+  RETURN_TO_RIGHT: "RETURN_TO_RIGHT",
 };
 
+const RETURN_SPEED_STANDARD = 10;
+const elementWidth = 500;
+
+// stores
+const mouseEnter = {
+  enterTime: 0,
+  leaveTime: 0,
+  enterX: 0,
+  leaveX: 0,
+  type: TYPE.STAY,
+};
 let position = 0;
 
-console.log(elScrollContainer);
+// methods
+const watchTypeChange = (type) => {
+  console.log(type);
+};
 
-// setInterval(() => {
-//   position += 100;
-//   elScrollContainer.style.transform = "translateX(-" + position + "%)";
-// }, 1000);
+const speedCalculator = (event) => {
+  // speed = time / distance
+  const time = event.leaveTime - event.enterTime;
+  const distance = Math.abs(event.leaveX - event.enterX);
+  return time / distance;
+};
 
-elScrollContainer.addEventListener("onmouseup");
-elScrollContainer.addEventListener("onmouseup");
+const eventTypeHandler = (event, dispatch) => {
+  const changedX = event.enterX - event.leaveX;
+  if (changedX === 0) {
+    event.type = TYPE.STAY;
+  } else if (changedX > 0) {
+    const speed = speedCalculator(event);
+    console.log(speed);
+    if (speed > RETURN_SPEED_STANDARD) {
+      event.type = TYPE.RETURN_TO_LEFT;
+    } else {
+      event.type = TYPE.LEFT;
+    }
+  } else {
+    const speed = speedCalculator(event);
+    console.log(speed);
+    if (speed > RETURN_SPEED_STANDARD) {
+      event.type = TYPE.RETURN_TO_RIGHT;
+    } else {
+      event.type = TYPE.RIGHT;
+    }
+  }
+  dispatch(event.type);
+};
+
+const mouseDownEventListener = (e) => {
+  mouseEnter.enterTime = e.timeStamp;
+  mouseEnter.enterX = e.x;
+};
+
+const mouseUpEventListener = (e) => {
+  mouseEnter.leaveTime = e.timeStamp;
+  mouseEnter.leaveX = e.x;
+
+  eventTypeHandler(mouseEnter, watchTypeChange);
+};
+
+// events
+elScrollContainer.addEventListener("mousedown", mouseDownEventListener);
+elScrollContainer.addEventListener("mouseup", mouseUpEventListener);
