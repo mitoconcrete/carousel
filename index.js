ef473b96ccca764b03ff3639c7dba244cf0ae499
Tae hoon Kim <taehoon.kim@fiflfifl.com>
2021-12-13 09:43:50 +0900

    add] touch events for modile-device working.

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ const RETURN_SPEED_STANDARD = 0.2;
 const MOVE_POSITION_STANDARD = 100;
 const LEFT_MAX = 0;
 const LEFT_BOUNCING_MAX = 10;
-const RIGHT_MAX = -200;
-const RIGHT_BOUNCING_MAX = -210;
+const RIGHT_MAX = -500;
+const RIGHT_BOUNCING_MAX = -510;
 
 // stores
 const mouseEnter = {
@@ -44,53 +44,53 @@ const watchTypeChange = (type) => {
   }
 };
 
-const speedCalculator = (event) => {
+const speedCalculator = (payload) => {
   // speed = distance / time
-  const time = event.leaveTime - event.enterTime;
-  const distance = Math.abs(event.leaveX - event.enterX);
+  const time = payload.leaveTime - payload.enterTime;
+  const distance = Math.abs(payload.leaveX - payload.enterX);
   return distance / time;
 };
 
-const eventTypeHandler = (event, dispatch) => {
-  const changedX = event.enterX - event.leaveX;
+const eventTypeHandler = (payload, dispatch) => {
+  const changedX = payload.enterX - payload.leaveX;
   if (changedX === 0) {
-    event.type = TYPE.STAY;
+    payload.type = TYPE.STAY;
   } else if (changedX > 0) {
-    const speed = speedCalculator(event);
+    const speed = speedCalculator(payload);
     if (speed < RETURN_SPEED_STANDARD) {
-      event.type = TYPE.RETURN_TO_RIGHT;
+      payload.type = TYPE.RETURN_TO_RIGHT;
     } else {
-      event.type = TYPE.RIGHT;
+      payload.type = TYPE.RIGHT;
     }
   } else {
-    const speed = speedCalculator(event);
+    const speed = speedCalculator(payload);
     if (speed < RETURN_SPEED_STANDARD) {
-      event.type = TYPE.RETURN_TO_LEFT;
+      payload.type = TYPE.RETURN_TO_LEFT;
     } else {
-      event.type = TYPE.LEFT;
+      payload.type = TYPE.LEFT;
     }
   }
-  dispatch(event.type);
+  dispatch(payload.type);
 };
 
 const mouseDownEventListener = (e) => {
   mouseEnter.enterTime = e.timeStamp;
-  mouseEnter.enterX = e.x;
+  mouseEnter.enterX = e.x || e.changedTouches[0].clientX;
   mouseEnter.isClicked = true;
 };
 
 const mouseUpEventListener = (e) => {
   mouseEnter.leaveTime = e.timeStamp;
-  mouseEnter.leaveX = e.x;
+  mouseEnter.leaveX = e.x || e.changedTouches[0].clientX;
   mouseEnter.isClicked = false;
   eventTypeHandler(mouseEnter, watchTypeChange);
 };
 
 const mouseMoveEventListener = (e) => {
+  const x = e.x || e.changedTouches[0].clientX;
   if (mouseEnter.isClicked) {
     const standardX = mouseEnter.enterX;
-    // position += standardX - e.x;
-    const movePercent = ((standardX - e.x) / 500) * 180;
+    const movePercent = ((standardX - x) / 500) * 180;
     let translatePercent = position + movePercent * -1;
     if (translatePercent < 0) {
       translatePercent = Math.max(translatePercent, RIGHT_BOUNCING_MAX);
@@ -102,8 +102,40 @@ const mouseMoveEventListener = (e) => {
   return;
 };
 
+const touchStartEventListener = (e) => {
+  mouseDownEventListener(e);
+};
+
+const touchEndEventListener = (e) => {
+  mouseUpEventListener(e);
+};
+
+const touchMoveEventListener = (e) => {
+  mouseMoveEventListener(e);
+};
+
+const isMobile = () => {
+  const UserAgent = navigator.userAgent;
+  if (
+    UserAgent.match(
+      /iPhone|iPod|Android|Windows CE|BlackBerry|Symbian|Windows Phone|webOS|Opera Mini|Opera Mobi|POLARIS|IEMobile|lgtelecom|nokia|SonyEricsson/i
+    ) != null ||
+    UserAgent.match(/LG|SAMSUNG|Samsung/) != null
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
 // events
-elScrollContainer.addEventListener("mousedown", mouseDownEventListener);
-elScrollContainer.addEventListener("mouseup", mouseUpEventListener);
-elScrollContainer.addEventListener("mousemove", mouseMoveEventListener);
-elScrollContainer.addEventListener("mouseleave", mouseUpEventListener);
+if (!isMobile()) {
+  elScrollContainer.addEventListener("mousedown", mouseDownEventListener);
+  elScrollContainer.addEventListener("mouseup", mouseUpEventListener);
+  elScrollContainer.addEventListener("mousemove", mouseMoveEventListener);
+  elScrollContainer.addEventListener("mouseleave", mouseUpEventListener);
+} else {
+  elScrollContainer.addEventListener("touchstart", touchStartEventListener);
+  elScrollContainer.addEventListener("touchend", touchEndEventListener);
+  elScrollContainer.addEventListener("touchmove", touchMoveEventListener);
+}
